ba8a1896c094e7a8050b42226a14dcae6cf2e01f
Oliver Hassett <oliverlouishassett@gmail.com>
2023-07-13 11:22:40 +1000

    update shell script reference for displaymenu

---
+++ .screenlayout/office.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 3840x2400 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --mode 3840x2160 --pos 3844x0 --rotate normal --output DP-4 --off
